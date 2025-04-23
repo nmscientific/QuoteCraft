7cae67845cf1148aa9d1ec2482033a6dd5968315
Jason Rentz <jasonrentz@gmail.com>
2025-04-23 16:36:38 +0000

    this page needs to be built

--- src/app/open-quote/page.tsx
+++ src/app/open-quote/page.tsx
@@ -1,6 +1,6 @@
 export default function OpenQuotePage() {
   return (
-    <div>
+    <div className="container py-10">
       <h1>Open Existing Quote</h1>
       <p>This is where you open and manage existing quotes.</p>
     </div>

ed90964da52906b233b2c052f5ca85cfeb5bacbb
Jason Rentz <jasonrentz@gmail.com>
2025-04-25 20:20:53 +0000

    can you make a header file that is included on all pages that has the logo as well as the company name 'B&T Glass, Inc." and the phone number "(575) 443-6261" so that the header file can be easily changed later without messing up the app code

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import Image from 'next/image';
+import logo from '@/public/logo.png';
+
+export default function Header() {
+  return (
+    <header className="bg-secondary p-4 flex items-center justify-between print:hidden">
+      <div className="flex items-center">
+        <Image src={logo} alt="B&T Glass, Inc. Logo" width={50} height={50} className="mr-4" />
+        <div>
+          <h1 className="text-xl font-bold">B&T Glass, Inc.</h1>
+        </div>
+      </div>
+      <div>
+        <p>(575) 443-6261</p>
+      </div>
+    </header>
+  );
+}
